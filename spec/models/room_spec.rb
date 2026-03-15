8bad5619e84a976974038ecf88a97d571cf34247
romanpelekh136 <rpeleh19@gmail.com>
2026-03-15 02:53:26 +0200

    Refactor room_spec to ensure user association in tests and validate unique room names

--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Room, type: :model do
+  let(:user) { create(:user) }
   it 'is valid with a name' do
     room = build(:room)
     expect(room).to be_valid
@@ -13,6 +14,13 @@ RSpec.describe Room, type: :model do
     expect(room.errors[:name]).to include("can't be blank")
   end
 
+  it 'is not valid with the same name as another room' do
+    create(:room, name: "Chess", user: user)
+    room = build(:room, name: "Chess", user: user)
+
+    expect(room).not_to be_valid
+  end
+
   it 'is not valid without a user' do
     room = build(:room, user: nil)
     room.valid?
@@ -21,8 +29,15 @@ RSpec.describe Room, type: :model do
   end
 
   it 'can have many messages' do
-    room = create(:room)
-    message = create(:message, room: room)
+    room = create(:room, user: user)
+    message = create(:message, room: room, user: user)
     expect(room.messages).to include(message)
+  end
+
+  it 'destroys associated message when Room is deleted' do
+    room = create(:room, user: user)
+    message = create(:message, room: room, user: user)
+
+    expect { room.destroy }.to change(Message, :count).by(-1)
   end
 end
